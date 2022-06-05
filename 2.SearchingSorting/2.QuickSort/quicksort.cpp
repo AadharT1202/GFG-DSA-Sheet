dd314d5ed16ad6639d48056176aa6b70af513bf0
AadharT1202 <aadhar.toshniwal12@gmail.com>
2022-06-05 21:52:06 +0530

    quick sort

---
+++ 2.SearchingSorting/2.QuickSort/quicksort.cpp
@@ -0,0 +1,51 @@
+#include<iostream>
+#include<vector>
+using namespace std;
+int partition (int arr[], int low, int high)
+{
+    int pivot = arr[high];
+    int i=low-1;
+
+    for(int j=low; j<high; j++)
+    {
+        if(arr[j]<pivot)
+        {
+            i++;
+            swap(arr[i],arr[j]);
+        }
+    }
+    i++;
+    swap(arr[i],arr[high]);
+    return i;
+}
+void quickSort(int arr[], int low, int high)
+{
+    if(low < high){
+        int p = partition(arr,low,high);
+        quickSort(arr,low,p-1);
+        quickSort(arr,p+1,high);
+    }
+}
+
+
+
+int main()
+{
+    int n;
+    cin>>n;
+    int arr[n];
+    for(int i=0; i<n; i++)
+        cin>>arr[i];
+    
+    cout<<"Before Sorting: ";
+    for(int i=0; i<n; i++)
+        cout<<arr[i]<<" ";
+
+    quickSort(arr,0,n-1);
+    cout<<endl;
+
+    cout<<"After Sorting: ";
+    for(int i=0; i<n; i++)
+        cout<<arr[i]<<" ";
+
+}
